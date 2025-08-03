ff6405bf2c1113106ac8f9c28661444116fefbf2
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2025-08-03 20:00:34 +0700

    Updated Login

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,12 +1,20 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    const value = {
+    //State variables to handle logins
 
+    const [aToken, setAtoken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const value = {
+        //so that can access toke for any components
+        aToken, setAtoken,
+        backendUrl
     }
 
     return (
